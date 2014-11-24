39f5bbcb63b9b49f306c4a2f1ee78fcce7fc47f2
feng124 <jifeng@pset.suntec.net>
2014-11-24 16:52:04 +0800

    Given an input string, reverse the string word by word.
    For example,
    Given s = "the sky is blue",
    return "blue is sky the". 
    
    Clarification:
    
    1. What constitutes a word?
        A sequence of non-space characters constitutes a word.
    2. Could the input string contain leading or trailing spaces?
        Yes. However, your reversed string should not contain leading or
    trailing spaces.
    3. How about multiple spaces between two words?
        Reduce them to a single space in the reversed string.

---
+++ src/oj/leetcode/string/ReverseWords.java
@@ -0,0 +1,57 @@
+package oj.leetcode.string;
+
+/**
+ *
+ * Given an input string, reverse the string word by word.
+ * 
+ * For example, Given s = "the sky is blue", return "blue is sky the".
+ * 
+ * Clarification:
+ * 
+ * 1. What constitutes a word? A sequence of non-space characters constitutes a
+ * word. 2. Could the input string contain leading or trailing spaces? Yes.
+ * However, your reversed string should not contain leading or trailing spaces.
+ * 3. How about multiple spaces between two words? Reduce them to a single space
+ * in the reversed string.
+ *
+ */
+public class ReverseWords {
+
+	public String reverseWords(String s) {
+		if (s == null || s.isEmpty()) {
+			return s;
+		}
+
+		StringBuffer buf = new StringBuffer();
+
+		int ws = s.length() - 1;
+		while (ws >= 0) {
+			int we = ws + 1;
+			for (; we > 0; we--) {
+				if (s.charAt(we - 1) != ' ') {
+					break;
+				}
+			}
+			if (we == 0) {
+				break;
+			}
+			
+			ws = we;
+			for (; ws > 0; ws--) {
+				if (s.charAt(ws - 1) == ' ') {
+					break;
+				}
+			}
+
+			if (ws < we) {
+				if (buf.length() > 0) {
+					buf.append(' ');
+				}
+				buf.append(s.substring(ws, we));
+			}
+			ws--;
+		}
+
+		return buf.toString();
+	}
+}
---
+++ src/oj/leetcode/string/ReverseWordsTest.java
@@ -0,0 +1,47 @@
+package oj.leetcode.string;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReverseWordsTest {
+	
+	private ReverseWords rw = null;
+	private String query = null;
+	private String guess = null;
+
+	@Test
+	public void test() {
+		rw = new ReverseWords();
+
+		query = "    ";
+		guess = rw.reverseWords(query);
+		assertEquals(guess, "");
+		
+		query = "s          ";
+		guess = rw.reverseWords(query);
+		assertEquals(guess, "s");
+		
+		query = "             s";
+		guess = rw.reverseWords(query);
+		assertEquals(guess, "s");
+		
+		query = "  s  ";
+		guess = rw.reverseWords(query);
+		assertEquals(guess, "s");
+		
+		query = "s";
+		guess = rw.reverseWords(query);
+		assertEquals(guess, "s");
+		
+		query = "the sky is blue";
+		guess = rw.reverseWords(query);
+		assertEquals(guess, "blue is sky the");
+		
+		query = "  the  sky is   blue     ";
+		guess = rw.reverseWords(query);
+		assertEquals(guess, "blue is sky the");
+		
+	}
+
+}
