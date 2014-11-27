4e3c476c25b65d8bf2d660cd63fe6fe7af76e938
feng124 <jifeng@pset.suntec.net>
2014-11-27 11:52:47 +0800

    Question:
    Given a singly linked list L: L0→L1→…→Ln-1→Ln,
    reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
    
    You must do this in-place without altering the nodes' values.
    
    For example,
    Given {1,2,3,4}, reorder it to {1,4,2,3}.

---
+++ src/oj/leetcode/list/reorder/ReorderList.java
@@ -0,0 +1,60 @@
+package oj.leetcode.list.reorder;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ *
+ * Given a singly linked list L: L0→L1→…→Ln-1→Ln, reorder it to:
+ * L0→Ln→L1→Ln-1→L2→Ln-2→…
+ * 
+ * You must do this in-place without altering the nodes' values.
+ * 
+ * For example, Given {1,2,3,4}, reorder it to {1,4,2,3}.
+ *
+ */
+public class ReorderList {
+
+	public ListNode reorderList(ListNode head) {
+		if (head == null || head.next == null || head.next.next == null) {
+			return head;
+		}
+
+		ListNode cur = head;
+		ListNode ref = head;
+		ListNode prev = null;
+		while (ref != null && ref.next != null) {
+			prev = cur;
+			cur = cur.next;
+			ref = ref.next.next;
+		}
+
+		if (ref != null) {
+			ref = cur.next;
+			cur.next = null;
+			cur = ref;
+		}else if (prev != null){
+			prev.next = null;
+		}
+		
+		final Deque<ListNode> stack = new ArrayDeque<ListNode>();
+		ref = cur;
+		while(ref != null) {
+			stack.push(ref);
+			ref = ref.next;
+		}
+		
+		cur = head;
+		while(!stack.isEmpty()) {
+			ref = stack.pop();
+			if (ref != null) {
+				ref.next = cur.next;
+				cur.next = ref;
+				cur = ref.next;
+			}
+		}
+
+		return head;
+	}
+
+}
---
+++ src/oj/leetcode/list/reorder/ReorderListTest.java
@@ -0,0 +1,71 @@
+package oj.leetcode.list.reorder;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReorderListTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		ReorderList rl = new ReorderList();
+
+		ListNode head = buildList(new int[] { 1, 2, 3, 4 });
+		ListNode answer = buildList(new int[] {1, 4, 2, 3});
+		ListNode guess = rl.reorderList(head);
+		assertEqual(guess, answer);
+		
+		head = buildList(new int[] {1, 2, 3, 4, 5});
+		answer = buildList(new int[] {1, 5, 2, 4, 3});
+		guess = rl.reorderList(head);
+		assertEqual(guess, answer);
+	}
+
+	private void assertEqual(ListNode guess, ListNode answer) {
+		ListNode cur = guess;
+		int guess_len = 0;
+		while(cur != null) {
+			guess_len++;
+			cur = cur.next;
+		}
+		
+		cur = answer;
+		int answer_len = 0;
+		while(cur != null) {
+			answer_len++;
+			cur = cur.next;
+		}
+		
+		assertEquals(guess_len, answer_len);
+		
+		cur = guess;
+		ListNode ref = answer;
+		while(cur != null) {
+			assertEquals(cur.val, ref.val);
+			cur = cur.next;
+			ref = ref.next;
+		}
+	}
+
+	private ListNode buildList(int[] cur) {
+		if (cur == null || cur.length == 0) {
+			return null;
+		}
+
+		ListNode head = new ListNode(cur[0]);
+		if (cur.length > 1) {
+			ListNode node = head;
+			for (int i = 1; i < cur.length; i++) {
+				node.next = new ListNode(cur[i]);
+				node = node.next;
+			}
+		}
+		return head;
+	}
+
+}
