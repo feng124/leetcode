ac0cd89ca0cb15a6ac8368bdbcdb1f0e4d917042
feng124 <jifeng@pset.suntec.net>
2014-12-02 17:03:44 +0800

    Question:
    Given a binary tree, return the preorder traversal of its nodes' values.
    
    For example:
    Given binary tree {1,#,2,3},
    
       1
        \
         2
        /
       3
    
    return [1,2,3].
    
    Note: Recursive solution is trivial, could you do it iteratively?

---
+++ src/oj/leetcode/tree/preorder/PreorderTraversal.java
@@ -0,0 +1,51 @@
+package oj.leetcode.tree.preorder;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ * 
+ * For example: Given binary tree {1,#,2,3},
+ * 
+ *    1
+ *     \
+ *      2
+ *     / 
+ *    3
+ * 
+ * return [1,2,3].
+ * 
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ *
+ */
+public class PreorderTraversal {
+	
+	private final Deque<TreeNode> stack = new ArrayDeque<TreeNode>();
+	
+	public List<Integer> preorderTraversal(TreeNode root) {
+		if (root == null) {
+			return Collections.emptyList();
+		}
+		
+		List<Integer> ret = new ArrayList<Integer>();
+		stack.clear();
+		stack.push(root);
+		while(!stack.isEmpty()) {
+			TreeNode cur = stack.pop();
+			ret.add(cur.val);
+			if (cur.right != null) {
+				stack.push(cur.right);
+			}
+			if (cur.left != null) {
+				stack.push(cur.left);
+			}
+		}
+		
+		return ret;
+	}
+}
---
+++ src/oj/leetcode/tree/preorder/PreorderTraversalTest.java
@@ -0,0 +1,87 @@
+package oj.leetcode.tree.preorder;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+
+import org.junit.Test;
+
+public class PreorderTraversalTest {
+
+	@Test
+	public void test() {
+
+		PreorderTraversal pt = new PreorderTraversal();
+		TreeNode root = buildBinTree(null);
+		List<Integer> preorder_list = pt.preorderTraversal(root);
+		assertEquals(preorder_list.size(), 0);
+
+		String[] toks = new String[] { "1", "2", "3", "#", "#", "4" };
+		root = buildBinTree(toks);
+		preorder_list = pt.preorderTraversal(root);
+		assertEquals(preorder_list.size(), 4);
+		assertEquals(preorder_list.get(0).intValue(), 1);
+		assertEquals(preorder_list.get(1).intValue(), 2);
+		assertEquals(preorder_list.get(2).intValue(), 3);
+		assertEquals(preorder_list.get(3).intValue(), 4);
+		
+		toks = new String[] { "1", "2", "3", "#", "#", "#", "4" };
+		root = buildBinTree(toks);
+		preorder_list = pt.preorderTraversal(root);
+		assertEquals(preorder_list.size(), 4);
+		assertEquals(preorder_list.get(0).intValue(), 1);
+		assertEquals(preorder_list.get(1).intValue(), 2);
+		assertEquals(preorder_list.get(2).intValue(), 3);
+		assertEquals(preorder_list.get(3).intValue(), 4);
+		
+		toks = new String[] { "1", "2", "4", "#", "#", "#", "3" };
+		root = buildBinTree(toks);
+		preorder_list = pt.preorderTraversal(root);
+		assertEquals(preorder_list.size(), 4);
+		assertEquals(preorder_list.get(0).intValue(), 1);
+		assertEquals(preorder_list.get(1).intValue(), 2);
+		assertEquals(preorder_list.get(2).intValue(), 4);
+		assertEquals(preorder_list.get(3).intValue(), 3);
+		
+		
+		toks = new String[] {"1", "#", "2", "3"};
+		root = buildBinTree(toks);
+		preorder_list = pt.preorderTraversal(root);
+		assertEquals(preorder_list.size(), 3);
+		assertEquals(preorder_list.get(0).intValue(), 1);
+		assertEquals(preorder_list.get(1).intValue(), 2);
+		assertEquals(preorder_list.get(2).intValue(), 3);
+	}
+
+	private TreeNode buildBinTree(String[] toks) {
+		if (toks == null || toks.length == 0) {
+			return null;
+		}
+
+		if (toks[0].equals("#")) {
+			throw new IllegalArgumentException();
+		}
+
+		final Deque<TreeNode> stack = new ArrayDeque<TreeNode>();
+		TreeNode root = new TreeNode(Integer.parseInt(toks[0]));
+		stack.push(root);
+		for (int i = 1; i < toks.length;) {
+			TreeNode cur = stack.pop();
+			if ((i + 1) < toks.length) {
+				if (!toks[i + 1].equals("#")) {
+					cur.right = new TreeNode(Integer.parseInt(toks[i + 1]));
+					stack.push(cur.right);
+				}
+			}
+			if (!toks[i].equals("#")) {
+				cur.left = new TreeNode(Integer.parseInt(toks[i]));
+				stack.push(cur.left);
+			}
+			i += 2;
+		}
+		return root;
+	}
+
+}
