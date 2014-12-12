c2b5133b392d9cd656d18b481c4b9c5a8932c799
feng124 <jifeng@pset.suntec.net>
2014-12-12 17:37:08 +0800

    Question：
    Given a binary tree, return the level order traversal of its nodes'
    values. (ie, from left to right, level by level).
    
    For example:
    Given binary tree {3,9,20,#,#,15,7},
    
        3
       / \
      9  20
        /  \
       15   7
    
    return its level order traversal as:
    
    [
      [3],
      [9,20],
      [15,7]
    ]
    
    confused what "{1,#,2,3}" means? > read more on how binary tree is
    serialized on OJ.
    
    OJ's Binary Tree Serialization:
    
    The serialization of a binary tree follows a level order traversal,
    where '#' signifies a path terminator where no node exists below.
    
    Here's an example:
    
       1
      / \
     2   3
        /
       4
        \
         5
    
    The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".

---
+++ src/oj/leetcode/tree/levelorder/LevelOrderTraversal.java
@@ -0,0 +1,77 @@
+package oj.leetcode.tree.levelorder;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 
+ * Given a binary tree, return the level order traversal of its nodes' values.
+ * (ie, from left to right, level by level).
+ * 
+ * For example: Given binary tree {3,9,20,#,#,15,7},
+ * 
+ *     3 
+ *    / \ 
+ *   9  20 
+ *      / \ 
+ *     15  7
+ * 
+ * return its level order traversal as:
+ * 
+ * [ 
+ *   [3], 
+ *   [9,20], 
+ *   [15,7] 
+ * ]
+ *
+ */
+public class LevelOrderTraversal {
+
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		final List<List<Integer>> ret = new ArrayList<List<Integer>>();
+		if (root == null) {
+		
+			final Deque<TreeEntry> queue = new ArrayDeque<TreeEntry>();
+			queue.addLast(new TreeEntry(root, 0));
+			while(!queue.isEmpty()) {
+				TreeEntry entry = queue.pollFirst();
+				
+				int depth = entry.depth;
+				TreeNode node = entry.node;
+				if (node.left != null) {
+					queue.addLast(new TreeEntry(node.left, depth+1));
+				}
+				if (node.right != null) {
+					 queue.addLast(new TreeEntry(node.right, depth+1));
+				}
+				
+				addEntry(ret, entry);
+			}
+		
+		}
+		return ret;
+	}
+	
+	private void addEntry(List<List<Integer>> res, TreeEntry entry) {
+		int depth = entry.depth;
+		if (depth == res.size()) {
+			res.add(new ArrayList<Integer>());
+		}
+		List<Integer> level_list = res.get(depth);
+		level_list.add(entry.node.val);
+	}
+
+	class TreeEntry {
+		
+		TreeNode node;
+		int depth;
+		
+		TreeEntry(TreeNode node, int depth) {
+			this.node = node;
+			this.depth = depth;
+		}
+		
+	}
+}
