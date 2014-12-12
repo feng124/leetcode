4c8b2163aaa3a847b122094dd6bebe5656b4d271
feng124 <jifeng@pset.suntec.net>
2014-12-12 16:44:10 +0800

    Question:
    Given a binary tree, find its maximum depth.
    
    The maximum depth is the number of nodes along the longest path from the
    root node down to the farthest leaf node.

---
+++ src/oj/leetcode/tree/maxdepth/MaxDepth.java
@@ -0,0 +1,57 @@
+package oj.leetcode.tree.maxdepth;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 
+ * Given a binary tree, find its maximum depth.
+ * 
+ * The maximum depth is the number of nodes along the longest path from the root
+ * node down to the farthest leaf node.
+ *
+ */
+public class MaxDepth {
+
+	public int maxDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+
+		int max_depth = Integer.MIN_VALUE;
+		final Deque<TreeEntry> stack = new ArrayDeque<TreeEntry>();
+		stack.push(new TreeEntry(root, 1));
+		while (!stack.isEmpty()) {
+			TreeEntry cur = stack.pop();
+			TreeNode node = cur.node;
+			if (node.left == null && node.right == null) {
+				int depth = cur.depth;
+				if (depth > max_depth) {
+					max_depth = depth;
+				}
+			}
+			if (node.left != null) {
+				int depth = cur.depth + 1;
+				stack.push(new TreeEntry(node.left, depth));
+			}
+			if (node.right != null) {
+				int depth = cur.depth + 1;
+				stack.push(new TreeEntry(node.right, depth));
+			}
+		}
+
+		return max_depth;
+	}
+	
+	class TreeEntry {
+
+		TreeNode node;
+		int depth;
+
+		TreeEntry(TreeNode node, int depth) {
+			this.node = node;
+			this.depth = depth;
+		}
+	}
+
+}
---
+++ src/oj/leetcode/tree/maxdepth/TreeNode.java
@@ -0,0 +1,15 @@
+package oj.leetcode.tree.maxdepth;
+
+public class TreeNode {
+	
+	int val;
+	TreeNode left;
+	TreeNode right;
+	
+	public TreeNode(int val) {
+		this.val = val;
+		this.left = null;
+		this.right = null;
+	}
+
+}
