2019c9ab727011152214cf4341577255a489a968
feng124 <jifeng@pset.suntec.net>
2014-12-16 21:00:47 +0800

    Question:
    Given a binary tree, check whether it is a mirror of itself (ie,
    symmetric around its center).
    
    For example, this binary tree is symmetric:
    
        1
       / \
      2   2
     / \ / \
    3  4 4  3
    
    But the following is not:
    
        1
       / \
      2   2
       \   \
       3    3
    
    Note:
    Bonus points if you could solve it both recursively and iteratively.

---
+++ src/oj/leetcode/tree/order/symmetric/SymmetricTree.java
@@ -0,0 +1,59 @@
+package oj.leetcode.tree.order.symmetric;
+
+import oj.leetcode.tree.TreeNode;
+
+/**
+ * 
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric
+ * around its center).
+ * 
+ * For example, this binary tree is symmetric:
+ * 
+ *        1
+ *       / \
+ *      2   2
+ *     / \ / \
+ *    3  4 4  3
+ * 
+ * But the following is not:
+ * 
+ *        1
+ *       / \
+ *      2   2
+ *       \   \
+ *        3   3
+ * 
+ * Note: Bonus points if you could solve it both recursively and iteratively.
+ *
+ */
+public class SymmetricTree {
+
+	public boolean isSymmetric(TreeNode root) {
+
+		if (root == null) {
+			return true;
+		}
+
+		return isSymmetric(root.left, root.right);
+	}
+
+	private boolean isSymmetric(TreeNode left, TreeNode right) {
+		if (left == null && right == null) {
+			return true;
+		}
+
+		if (left != null && right != null) {
+			if (left.val != right.val) {
+				return false;
+			}
+			
+			boolean symmetric = isSymmetric(left.left, right.right);
+			if (symmetric) {
+				symmetric = isSymmetric(left.right, right.left);
+			}
+			return symmetric;
+		}
+		return false;
+
+	}
+}
---
+++ src/oj/leetcode/tree/order/symmetric/SymmetricTreeTest.java
@@ -0,0 +1,32 @@
+package oj.leetcode.tree.order.symmetric;
+
+import static org.junit.Assert.*;
+import oj.leetcode.tree.TreeBuilder;
+import oj.leetcode.tree.TreeNode;
+
+import org.junit.Test;
+
+public class SymmetricTreeTest {
+	
+	private final SymmetricTree inst = new SymmetricTree();
+
+	@Test
+	public void test() {
+		TreeNode tree = TreeBuilder.buildBinaryTree(new String[] {"1", "2", "2", "3", "4", "4", "3"});
+		boolean symmetric = inst.isSymmetric(tree);
+		assertTrue(symmetric);
+		
+		tree = TreeBuilder.buildBinaryTree(new String[] {"1", "2", "2", "#", "4", "4", "#"});
+		symmetric = inst.isSymmetric(tree);
+		assertTrue(symmetric);
+		
+		tree = TreeBuilder.buildBinaryTree(new String[] {"1", "2", "2", "3", "#", "#", "3"});
+		symmetric = inst.isSymmetric(tree);
+		assertTrue(symmetric);
+		
+		tree = TreeBuilder.buildBinaryTree(new String[] {"1", "2", "2", "3", "4", "4", "#"});
+		symmetric = inst.isSymmetric(tree);
+		assertFalse(symmetric);
+	}
+
+}
